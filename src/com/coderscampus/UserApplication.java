870b69d08f5137f6f1643e2f81356b60226e7dc9
trinareyes <katrinajreyes@outlook.com>
2024-05-26 00:05:30 -0700

    updated my userapplication

--- src/com/coderscampus/UserApplication.java
+++ src/com/coderscampus/UserApplication.java
@@ -8,8 +8,8 @@ public class UserApplication {
 		Scanner scanner = new Scanner(System.in);
 		UserService userService = new UserService();
 		
-		
-		System.out.println("Enter your username");
+		User[] users = userService.getUsersFromFile();
+		System.out.println("Enter your username:3");
 		String username = scanner.nextLine();
 		
 		
@@ -17,11 +17,20 @@ public class UserApplication {
 		String password = scanner.nextLine();
 		
 		
-	
 		
 		String[] stringInput = new String[2];
 		stringInput[0] = username;
 		stringInput[1] = password;
+		
+		
+		for (User user : users) {
+			if (user != null && user.getUsername().equals(username) && user.getPassword().equals(password)) {
+				System.out.println(" " + user.getName());
+				break;
+			}
+		}
+		
+		
 		
 		User user = userService.createUser(stringInput);
 		System.out.println("User created: " + user);
