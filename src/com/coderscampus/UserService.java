4a27b556e42d79512bf6899e251d4a5230f9624a
trinareyes <katrinajreyes@outlook.com>
2024-05-25 20:01:49 -0700

    updated userservice
    it should compile now

--- src/com/coderscampus/UserService.java
+++ src/com/coderscampus/UserService.java
@@ -28,9 +28,16 @@ public class UserService {
 			}
 		}
 		
-		
-		
-		
-					
 	}
-}
+
+		   public User createUser(String[] stringInput) {
+		        User user = new User();
+		        user.setUsername(stringInput[0]);
+		        user.setPassword(stringInput[1]);
+		        user.setName(stringInput[2]);
+		        return user;
+		    }		
+		
+		
+			
+	}
